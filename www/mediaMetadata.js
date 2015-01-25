e1e3c64763ed799c2a479b73305dae01bc230203
Jeremy Plack <stlouisweb@gmail.com>
2015-01-25 15:41:09 -0600

    Fixes capitaliztion errors.

--- www/mediaMetadata.js
+++ www/mediaMetadata.js
@@ -3,7 +3,7 @@ var mediaMeta = {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'MediaMeta', // mapped to our native Java class called "CalendarPlugin"
+            'MediaMetadata', // mapped to our native Java class called "CalendarPlugin"
             'getMediaMetadata', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "fullpath": fullpath
