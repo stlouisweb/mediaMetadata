ea326fac1215dd954b939596be4bb23c9e23319c
Jeremy Plack <stlouisweb@gmail.com>
2015-01-25 21:07:05 -0600

    add track info to callback.

--- www/mediaMetadata.js
+++ www/mediaMetadata.js
@@ -2,7 +2,7 @@
 window.MediaMeta = {
     createEvent: function(fullpath, successCallback, errorCallback) {
         cordova.exec(
-            successCallback, // success callback function
+            successCallback(trackinfo), // success callback function
             errorCallback, // error callback function
             'MediaMetadata', // mapped to our native Java class called "MediaMetadata"
             'getMediaMetadata', // with this action name
