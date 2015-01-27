1c9f0fabaa69663237303a4ba33d880698173f14
Jeremy Plack <stlouisweb@gmail.com>
2015-01-26 20:22:02 -0600

    adds message to error callback.

--- www/mediaMetadata.js
+++ www/mediaMetadata.js
@@ -1,10 +1,13 @@
 
 window.MediaMeta = {
     createEvent: function(fullpath, successCallback, errorCallback) {
-        cordova.exec(function(trackinfo){
-            trackinfo = trackinfo;
-            successCallback(trackinfo)}, // success callback function
-            errorCallback, // error callback function
+        cordova.exec(
+            function(trackinfo){
+                trackinfo = trackinfo;
+                successCallback(trackinfo)}, // success callback function
+            function(message){
+                message = message;
+                errorCallback(message)}, // error callback function
             'MediaMetadata', // mapped to our native Java class called "MediaMetadata"
             'getMediaMetadata', // with this action name
             [{                  // and this array of custom arguments to create our entry
