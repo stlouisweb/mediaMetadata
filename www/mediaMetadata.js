f4968612db65c7e02617fe0b2aa9b445da07f9a9
Jeremy Plack <stlouisweb@gmail.com>
2015-01-26 19:42:50 -0600

    modifies callback function.

--- www/mediaMetadata.js
+++ www/mediaMetadata.js
@@ -1,8 +1,9 @@
 
 window.MediaMeta = {
     createEvent: function(fullpath, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback(trackinfo), // success callback function
+        cordova.exec(function(trackinfo){
+            trackinfo = trackinfo;
+            successCallback(trackinfo)}, // success callback function
             errorCallback, // error callback function
             'MediaMetadata', // mapped to our native Java class called "MediaMetadata"
             'getMediaMetadata', // with this action name
